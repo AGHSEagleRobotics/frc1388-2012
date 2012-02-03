26fb4821f4190e035f40cf4609faa21d5e386d01
jarrett <jarrett@corr.org>
2012-02-03 03:21:12 +0000

    I put in the proper code for the Fire.cpp and Shooter.h and Shotter.cpp into the repository.

--- 2012RobotCode/Subsystems/Shooter.cpp
+++ 2012RobotCode/Subsystems/Shooter.cpp
@@ -1,16 +1,87 @@
 #include "Shooter.h"
 #include "../Robotmap.h"
+#include "../OI.h"
 
-Shooter::Shooter() : Subsystem("Shooter") {
-	
+Shooter::Shooter() : Subsystem("Shooter") 
+{
+	topAxel = new Victor(TOP_AXEL_VICTOR);
+	bottomAxel = new Victor(BOTTOM_AXEL_VICTOR);
+	topAxelEncoder = new Encoder(TOP_AXEL_ENCODER_A,TOP_AXEL_ENCODER_B, false, Encoder::k4X);
+	bottomAxelEncoder = new Encoder(BOTTOM_AXEL_ENCODER_A,BOTTOM_AXEL_ENCODER_B, false, Encoder::k4X);
+	pidTopAxel = new PIDController(0.0, 0.0, 0.0, topAxelEncoder, topAxel);
+	pidTopAxel = new PIDController(0.0, 0.0, 0.0, bottomAxelEncoder, bottomAxel);
+	ballDetect = new AnalogChannel(BALL_DETECT);
+	conveyorBelt = new Victor(CONVEYOR_BELT_VICTOR);
+	passed = new DigitalInput(BALL_PASSED);
 }
     
-void Shooter::InitDefaultCommand() {
+void Shooter::InitDefaultCommand()
+{
 	// Set the default command for a subsystem here.
 	//SetDefaultCommand(new MySpecialCommand());
 }
 
-void Shooter::BallStop()
+void Shooter::SetRange(float distance)
 {	
-	// The ball stopper is going to be controlled by this function.
+	float speed, m = 0.6981, b = 16.5255;
+	float fps_to_rpm = 0.034907, max_rpm = 2500.0;
+	
+	// use the distance (feet) to calculate the rpm of the top and bottom axel to
+	// achieve the proper velocity to make a basket
+	speed = ((m * distance) + b) / fps_to_rpm;
+	SetTopAxel(speed/max_rpm);
+	SetBottomAxel (speed/max_rpm);
+}
+
+void Shooter::TopAxelPID()
+{
+	// we need to figure out the PID values
+	pidTopAxel->SetPID(0, 0, 0);
+
+	pidTopAxel->SetOutputRange(0 , 1);
+	
+	topAxelEncoder->SetDistancePerPulse(0.06981);
+}
+
+void Shooter::BottomAxelPID()
+{
+	// we need to figure out the PID values
+	pidBottomAxel->SetPID(0, 0, 0);
+	
+	pidBottomAxel->SetOutputRange(0 , 1);
+	
+	bottomAxelEncoder->SetDistancePerPulse(0.06981);	
+}
+
+void Shooter::SetTopAxel(float fShooterSpeed)
+{
+	pidTopAxel->SetSetpoint(fShooterSpeed);
+}
+
+void Shooter::SetBottomAxel(float fShooterSpeed)
+{
+	pidBottomAxel->SetSetpoint(fShooterSpeed);
+}
+
+void Shooter::MoveConveyor()
+{
+		conveyorBelt->Set(0.5);
+}
+
+bool Shooter::IsBallPrimed()
+{
+	if(ballDetect->GetVoltage() < 0.04)
+	{
+		return true;
+	}
+	else
+	{	
+		return false;
+	}
+}
+
+bool Shooter::Passed()
+{
+	if(passed)
+		return true;
 }
--- 2012RobotCode/Subsystems/Shooter.h
+++ 2012RobotCode/Subsystems/Shooter.h
@@ -1,24 +1,36 @@
 #ifndef SHOOTER_H
 #define SHOOTER_H
+#include "Commands/Subsystem.h"
 #include "WPILib.h"
 
 class Shooter: public Subsystem {
 private:
-		Victor topAxel;
-		Victor bottomAxel;
-		Encoder topEncoder;
-		Encoder bottomEncoder;
-		PIDController pidTopAxel;
-		PIDController pidBottomAxel;
-		float fShooterSpeed;
-//		Victor ballStop;
-		DigitalInput openStop;
-		DigitalInput closeStop;
+	Victor *topAxel;
+	Victor *bottomAxel;
+	Encoder *topAxelEncoder;
+	Encoder *bottomAxelEncoder;
+	PIDController *pidTopAxel;
+	PIDController *pidBottomAxel;
+	AnalogChannel *ballDetect;
+	DigitalInput *passed;
+	Victor *conveyorBelt;
 		
 public:
 	Shooter();
 	void InitDefaultCommand();
-	void ballStop();
+	void SetRange(float distance);
+	void TopAxelPID();
+	void BottomAxelPID();
+	void SetTopAxel(float fShooterSpeed);
+	void SetBottomAxel(float fShooterSpeed);
+	void MoveConveyor();
+	bool IsBallPrimed();
+	bool Passed();
+	
+
+//	DigitalInput *openedStop;
+//	DigitalInput *closedStop;
+	
 };
 
 #endif
