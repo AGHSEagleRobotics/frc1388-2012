f01bcf434f7e354e3868beb152744da0723e7e4c
jarrett@corr.org <jarrett@corr.org@ff371526-8658-dfef-e933-5ba152a1f712>
2012-02-03 04:33:01 +0000

    Commented on the AutoAim Command and put thoughts down for the logic of the command.

--- trunk/2012RobotCode/Commands/AutoAim.cpp
+++ trunk/2012RobotCode/Commands/AutoAim.cpp
@@ -18,6 +18,10 @@ void AutoAim::Execute()
 {
 	// it will move the turret to stay locked on to the turret so the turret will
 	// be controlled by the vision system outputs
+	// target = vision->getNormalizedXPosition();
+	// the constant that the target is being multiplied by is subject to cahnge as
+	// it does not precisely reflect the p value for turning the turret
+	// turretMtr->Set((target * (4/10000)));
 }
 
 // Make this return true when this Command no longer needs to run execute()
