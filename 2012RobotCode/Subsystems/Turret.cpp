c9617a5198ac01d7c026102f70616a75d7461c22
joelmann2 <Joelmann2@gmail.com>
2012-02-03 02:50:51 +0000

    Turret Subsystems and Commands added to respective folders.

--- 2012RobotCode/Subsystems/Turret.cpp
+++ 2012RobotCode/Subsystems/Turret.cpp
@@ -2,7 +2,8 @@
 #include "../Robotmap.h"
 #include "SmartDashboard/SmartDashboard.h"
 
-Turret::Turret() : PIDSubsystem("Turret", Kp, Ki, Kd) {
+Turret::Turret() : PIDSubsystem("Turret", Kp, Ki, Kd) 
+{
 	// Use these to get going:
 	// SetSetpoint() -  Sets where the PID controller should move the system
 	//                  to
@@ -11,36 +12,44 @@ Turret::Turret() : PIDSubsystem("Turret", Kp, Ki, Kd) {
 	turretMtr = new Victor(DEFAULT_ANALOG_MODULE, TURRET_VICTOR_ID );
 	turretEncdr = new Encoder(TURRET_ENCODER_A, TURRET_ENCODER_B, true, Encoder::k4X);
 	turretGyro = new Gyro(DEFAULT_ANALOG_MODULE, TURRET_GYRO_ID);
+	
+	SetSetpoint(0);
+	Enable();
 }
 
-double Turret::ReturnPIDInput() {
-	// Return your input value for the PID loop
-	// e.g. a sensor, like a potentiometer:
-	// yourPot->SetAverageVoltage() / kYourMaxVoltage;
-	return 0.0;
+double Turret::ReturnPIDInput() 
+{
+	return turretGyro->GetAngle();
+
 }
 
-// float Turret::Get
 
-void Turret::UsePIDOutput(double output) {
+
+void Turret::UsePIDOutput(double output) 
+{
 	// Use output to drive your system, like a motor
 	// e.g. yourMotor->Set(output);
 	// 
+	
 	turretMtr->Set(output);
 }
 
-void Turret::InitDefaultCommand() {
+void Turret::InitDefaultCommand() 
+{
 	// Set the default command for a subsystem here.
 	//setDefaultCommand(new MySpecialCommand());
 }
-void Turret::TurnRelative(){
+void Turret::TurnRelative(double angle){
 	//TurnRelative is a function for specifying a turn by number of 
 	// degrees, i.e. "turn turret -5 degrees."
+	double currentAngle;
 	
+	currentAngle = turretGyro->GetAngle();
+	SetSetpoint((currentAngle + angle)%360.0);
 }
-void Turret::TurnAbsolute(){
-	//TurnAbsolute is a function for specifying a specific degree,
-	// i.e. "turn turret to 5 degrees."
+void Turret::TurnAbsolute(double angle){
+	SetSetpoint(angle);
+
 }
 
 
