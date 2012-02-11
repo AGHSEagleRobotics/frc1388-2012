4f87fc83303e1d65ad999f2d5a9b6e231a02384a
jarrett@corr.org <jarrett@corr.org@ff371526-8658-dfef-e933-5ba152a1f712>
2012-02-11 18:01:08 +0000

    Jarrett code

--- trunk/2012RobotCode/Commands/Fire.cpp
+++ trunk/2012RobotCode/Commands/Fire.cpp
@@ -13,37 +13,8 @@ void Fire::Initialize()
 // Called repeatedly when this Command is scheduled to run
 void Fire::Execute()
 {
-	// design has decided not to use a pice of metal to stop the ball before it
-	// enters the shooter so the code below in comments is obsolete and the 
-	// conveyer will turn off and on to control the balls that go into the shooter.
-	
-//	bool trigger = oi->getOpsStick()->GetButton(Joystick::kTriggerButton);
-//	bool opened = shooter->openedStop.Get();
-//	bool closed = shooter->closedStop.Get();
-//	float ballStop = shooter->ballStop.Get();
-//	bool passed = shooter->passed.Get();
-//	if(trigger)
-//	{
-//		if(closed)
-//		{
-//			ballStop = -0.75;
-//		}
-//		
-//		if(opened)
-//		{
-//			ballStop = 0;
-//		}
-//	
-//		if(passed)
-//		{
-//			ballStop = 0.75;
-//		}
-//		
-//	}
-	
 	shooter->MoveConveyor();
-	
-	
+	printf("FIRE \n");
 }
 
 // Make this return true when this Command no longer needs to run execute()
