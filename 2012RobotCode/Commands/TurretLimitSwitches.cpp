d78cf2636c62122b9525222e8c2b53c818132437
jarrett <jarrett@corr.org>
2012-02-18 23:20:50 +0000

    This has working upslot and downslot. Fixed the BallCollection code, so the sweepers work. This code can run the sweepers and the elevator to collect balls. And It Builds. -David

--- 2012RobotCode/Commands/TurretLimitSwitches.cpp
+++ 2012RobotCode/Commands/TurretLimitSwitches.cpp
@@ -15,12 +15,12 @@ void TurretLimitSwitches::Initialize() {
 // Called repeatedly when this Command is scheduled to run
 void TurretLimitSwitches::Execute() {
 	
-	if (turret->isLlimit() && turret->getMotor() < 0 ){
-			turret->setMotor(0);
-	}
-	else if (turret->isRlimit() && turret->getMotor() > 0 ){
-			turret->setMotor(0);
-	}
+//	if (turret->isLlimit() && turret->getMotor() < 0 ){
+//			turret->setMotor(0);
+//	}
+//	else if (turret->isRlimit() && turret->getMotor() > 0 ){
+//			turret->setMotor(0);
+//	}
 
 }
 // Make this return true when this Command no longer needs to run execute()
